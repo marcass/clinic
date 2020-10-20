7bb9db56163f3a7ae0d15fbe4d747f3fa7a77cc9
marcass <best.nose@gmail.com>
2020-10-21 12:33:24 +1300

    making priogress

--- python/vet_emials.py
+++ python/vet_emials.py
@@ -1,3 +1,32 @@
+# https://medium.com/swlh/how-to-scrape-email-addresses-from-a-website-and-export-to-a-csv-file-c5d1becbd1a0
+
+# Open asp.net form 9button is called "find a vet"
+# Opens on page '1', to interate pages links are in the style of <a title="Go to page 3" href="javascript:__doPostBack('ctl01$TemplateBody$WebPartManager1$gwpciNewQueryMenuCommon$ciNewQueryMenuCommon$ResultsGrid$Grid1$ctl00$ctl03$ctl01$ctl09','')"><span>3</span></a>
+# parse each page of the table looking for links starting with "href=/FindAVet?ID1125" as an exxample
+
+# Or: URL for each vet in form of https://www.vetcouncil.org.nz/FindAVet?ID=4197 (number is registratioin number) so could iterate through numbers up to 20000 and dump info if available
+# Email field is: 
+
+
+
+page = requests.get("https://www.vetcouncil.org.nz/FindAVet?ID=4196")
+soup = BeautifulSoup(page.content, 'html.parser')
+out = list(soup.children)
+content = out[3]
+content = list(content.children)
+content = content[3] #next layer
+content = list(content.children)
+content = content[3] #next layer
+content = list(content.children)
+content = content[3] #next layer
+content = list(content.children)
+#name and email is in content[21] here
+name = list(content[21].children) # need to extract it from here
+name = list(name[3].children)
+name = list(name[1].children)
+
+
+
 import re
 import requests
 from urllib.parse import urlsplit
