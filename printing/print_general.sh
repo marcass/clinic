8d4c5f0b8e53617cdee41fcd3d1b7a83ebfeb5fc
marcass <best.nose@gmail.com>
2020-12-04 11:17:47 +1300

    updated for general use

--- printing/print_general.sh
+++ printing/print_general.sh
@@ -1,7 +1,31 @@
 #! /bin/bash
 
+# Create a service unit in /etc/systemd/user/
+# 
+# 
+# [Unit]
+# Description=Printing service for ezyvet docs
+
+# [Service]
+# Type=simple
+# ExecStart=/bin/bash -c "/home/mw/git//clinic/printing/print_general.sh"
+
+# [Install]
+# WantedBy=default.target
+
+# 
+# 
+
+# start service by systemctl --user start ezyvet_doc_printing.service
+
 # Make sure this directory exists
-DOC="/home/mw/Downloads/printing/"
+DOC="$HOME/Downloads/printing/"
+if [ -d "$DOC" ]; then
+   echo "'$DOC' found and now copying files, please wait ..."
+else
+   echo "Warning: '$DOC' NOT found, creating $DOC."
+   mkdir $DOC
+fi
 
 inotifywait -m -e create $DOC |
 while read -r  events filename; do
